bfa459fa99e928cafaf66a3982521db0d4b93370
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-12 14:11:08 -0500

    navigation started

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Container from "@mui/material/Container";
+import Navigation from "../../Components/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Container maxWidth="lg">
+          <Navigation />
+          {children}
+        </Container>
+      </body>
     </html>
   );
 }
