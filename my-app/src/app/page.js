b6827cc0bc7d8ceb0ac568725dfa6f2afc7f127e
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-12 17:32:37 -0500

    showing Data

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,48 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 
 export default function page() {
+  const [topLosers, setTopLosers] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch(`http://localhost:3000/api/secondroute`);
+
+        if (!response.ok) {
+          throw new Error("failed to get data");
+        }
+        const result = await response.json();
+
+        setTopLosers(result.top_losers);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <div>hello from home page</div>
+      <h2>Here are the top losers:</h2>
+      <>
+        {topLosers.map((item, i) => (
+          <div key={i}>
+            <div>
+              {item.ticker} currently at {item.price} a change of{" "}
+              {item.change_amount}
+            </div>
+          </div>
+        ))}
+      </>
     </>
   );
 }
