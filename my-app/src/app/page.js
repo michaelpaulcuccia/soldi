b091ccbd954e43577986307a4649b70d37c2a3cf
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-12 14:00:38 -0500

    installs material ui

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default function page() {
+  return (
+    <>
+      <div>
+        hello <Button variant="text">Text</Button>
+      </div>
+    </>
+  );
+}
