126dfed3f12ff050c87ae46cdecd9536e85c70fe
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-16 15:17:48 -0500

    mobile menu, may need vertical tweeking

--- my-app/Components/UtilBar.jsx
+++ my-app/Components/UtilBar.jsx
@@ -1,10 +1,21 @@
 "use client";
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 import Link from "next/link";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { mobileBreakpoint } from "../constants";
 import { Alumni_Sans } from "next/font/google";
+
+const slideInFromRight = keyframes`
+  from {
+    transform: translateX(100%); /* Start off-screen to the right */
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0); /* End at its original position */
+    opacity: 1;
+  }
+`;
 
 const alumni = Alumni_Sans({
   subsets: ["latin"],
@@ -39,12 +50,32 @@ const MobileRoot = styled.div`
 
   @media (max-width: ${mobileBreakpoint}) {
     display: flex;
-    justify-content: flex-end;
+    flex-direction: column;
+    align-items: flex-end;
     padding: 12px 16px;
   }
 `;
 
+const MobileMenu = styled.div`
+  height: 94vh;
+  width: 100%;
+  background-color: #353333;
+  color: white;
+  animation: ${slideInFromRight} 0.5s ease forwards;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 50px 0;
+`;
+
 export default function UtilBar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowMobileMenu(!showMobileMenu);
+    console.log("click");
+  };
   return (
     <>
       <DesktopRoot className={alumni.className}>
@@ -58,8 +89,43 @@ export default function UtilBar() {
         <StyledLink href="/">Support</StyledLink>
       </DesktopRoot>
       <MobileRoot>
-        <RxHamburgerMenu color="white" fontSize={32} />
+        <RxHamburgerMenu onClick={handleClick} color="white" fontSize={32} />
+        {showMobileMenu && (
+          <MobileMenu onClick={handleClick}>
+            <StyledLink href="/">Home</StyledLink>
+            <StyledLink href="/planning">Planning</StyledLink>
+            <StyledLink href="/advisory">Advisory</StyledLink>
+            <StyledLink href="/individualsandfamilies">
+              Individuals and Families
+            </StyledLink>
+            <StyledLink href="/business">Business</StyledLink>
+            <StyledLink href="/">Our Company</StyledLink>
+            <StyledLink href="/">Support</StyledLink>
+          </MobileMenu>
+        )}
       </MobileRoot>
     </>
   );
 }
+
+/*
+
+const slideOutToRight = keyframes`
+  from {
+    transform: translateX(0); 
+    opacity: 1;
+  }
+  to {
+    transform: translateX(100%); 
+    opacity: 0;
+  }
+`;
+
+ animation: ${(props) =>
+    props.isOpen
+      ? `${slideInFromRight} 0.5s ease forwards`
+      : `${slideOutToRight} 0.5s ease forwards`};
+
+             <MobileMenu onClick={handleClick} isOpen={showMobileMenu}>
+
+*/
