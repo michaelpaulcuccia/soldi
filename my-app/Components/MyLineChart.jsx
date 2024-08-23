9872e588f8ba5930be6f36fbdf76876a5390e169
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-23 15:22:10 -0500

    modifying linechart

--- my-app/Components/MyLineChart.jsx
+++ my-app/Components/MyLineChart.jsx
@@ -2,19 +2,28 @@ import * as React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 
 export default function MyLineChart({ data }) {
-  const xValues = data.map((_, index) => index + 1);
+  const xValues = data.map((_, index) => index + 1); //1, 2, 3, 4...
+  //yValues need to be reversed
   const yValues = data.map((item) => item.reportedEPS);
+  const yValuesReversed = yValues.slice().reverse();
 
   return (
-    <LineChart
-      xAxis={[{ data: xValues }]}
-      series={[
-        {
-          data: yValues,
-        },
-      ]}
-      width={700}
-      height={500}
-    />
+    <>
+      <LineChart
+        xAxis={[
+          {
+            data: xValues,
+          },
+        ]}
+        series={[
+          {
+            data: yValuesReversed,
+          },
+        ]}
+        width={700}
+        height={500}
+      />
+      {}
+    </>
   );
 }
